3d93e09691f54148a2bfd3716e307074b7d7e71d
Francesc Arpi <francesc.arpi@gmail.com>
2016-05-25 07:00:20 +0200

    Working with unit testing

--- __tests__/I18n-test.js
+++ __tests__/I18n-test.js
@@ -3,14 +3,25 @@ jest.unmock('../src/component');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
+import {Provider} from "react-redux"
+import {createStore, applyMiddleware} from "redux"
+import {combineReducers} from "redux"
+
 import I18n from '../src/component';
+import {i18nState} from "../src/reducer"
+
 
 describe("I18n", () => {
   it("instantiate component", () => {
 
-    // const i18nComp = TestUtils.renderIntoDocument(
-    //   <I18n/>
-    // )
+    const reducer = combineReducers({ i18nState })
+    const store = createStore(reducer)
+
+    const i18nComp = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <I18n/>
+      </Provider>
+    )
 
     // const i18nNode = ReactDOM.findDOMNode(i18nComp)
     //
