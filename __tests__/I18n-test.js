e641e442f5f1630bd0c516fce566bd8b11c02265
Francesc Arpi <francesc.arpi@gmail.com>
2016-05-25 06:36:10 +0200

    Working with unit testing

---
+++ __tests__/I18n-test.js
@@ -0,0 +1,19 @@
+jest.unmock('../src/component');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import I18n from '../src/component';
+
+describe("I18n", () => {
+  it("instantiate component", () => {
+
+    // const i18nComp = TestUtils.renderIntoDocument(
+    //   <I18n/>
+    // )
+
+    // const i18nNode = ReactDOM.findDOMNode(i18nComp)
+    //
+    // console.log(i18nNode.textContent);
+  })
+})
