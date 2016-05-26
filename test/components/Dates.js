e6639297a33f57f7c0461b5c6bda68dc1eb94ff8
Francesc Arpi <francesc.arpi@gmail.com>
2016-05-26 21:32:29 +0200

    Working with dates

---
+++ test/components/Dates.js
@@ -0,0 +1,24 @@
+import React from "react"
+import moment from "momentjs"
+
+class Dates extends React.Component {
+
+  render() {
+
+    let sample = moment("2016-01-01")
+
+    return (
+      <div>
+        {this.context.d1(sample)}@@
+        {this.context.d2(sample)}
+      </div>
+    )
+  }
+}
+
+Dates.contextTypes = {
+  d1: React.PropTypes.func.isRequired,
+  d2: React.PropTypes.func.isRequired
+}
+
+export default Dates
