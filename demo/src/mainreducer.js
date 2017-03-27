9d905b69c8752c26f557ba1b028e6b2b7b437339
Francesc Arpi <farpi@apsl.net>
2017-03-27 16:34:00 +0200

    Demo for async translations

--- demo/src/mainreducer.js
+++ demo/src/mainreducer.js
@@ -2,14 +2,35 @@ import React from 'react'
 
 import {connect} from 'react-redux'
 
-import {setLanguage} from 'redux-i18n'
+import {setLanguage, setTranslations} from 'redux-i18n'
 
 class MainReducer extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.getRemoteTranslations = this.getRemoteTranslations.bind(this)
+  }
+
+  componentWillMount() {
+    this.props.dispatch(setLanguage('es'))
+  }
+
+  getRemoteTranslations() {
+    this.props.dispatch(setTranslations({
+      'es': {
+        'Hello world!': 'Hola Mundo!'
+      }
+    }))
+  }
+
   render() {
     return (
       <div>
         <h1>Async Translations</h1>
         Language: {this.props.lang}<br/>
+        Translation: {this.context.t('Hello world!')}<br/>
+        <button onClick={this.getRemoteTranslations}>Get remote translations</button><br/>
+        <pre style={{ background: '#e9e9e9' }}>{JSON.stringify(this.props.translations)}</pre>
       </div>
     )
   }
@@ -20,5 +41,6 @@ MainReducer.contextTypes = {
 }
 
 export default connect(state => ({
-  lang: state.i18nState.lang
+  lang: state.i18nState.lang,
+  translations: state.i18nState.translations
 }))(MainReducer)
