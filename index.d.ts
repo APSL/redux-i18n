c4c1f1c8137e693a704fbb84a7a5a088313ead8f
yosef <maulerik@gmail.com>
2017-10-02 05:34:38 +0300

    added typings

---
+++ index.d.ts
@@ -0,0 +1,64 @@
+declare module 'redux-i18n' {
+    import * as React from 'react';
+    import {Action} from 'redux';
+    import {ThunkAction} from 'redux-thunk';
+
+    export interface I18nProps {
+        translations: object;
+        useReducer?: boolean;
+        initialLang?: string;
+        fallbackLang?: string;
+        initialized?: boolean;
+    }
+
+    export interface IreduxI18nState {
+        lang: string;
+        translations: object;
+        forceRefresh: boolean;
+    }
+
+    export interface ISetLanguageAction extends Action {
+        lang: string;
+    }
+
+    export interface ISetForceRefreshAction extends Action {
+        force: boolean;
+    }
+
+    export interface IWrapWithLocalized {
+        (WrappedComponent: React.Component): Function;
+    }
+
+    export interface IGetTranslateFunctionResponse {
+        (textKey: string, params?: string[], comment?: string): string;
+    }
+
+    export interface ITranslation {
+        [name: string]: string;
+    }
+
+    export interface ITranslations {
+        [name: string]: ITranslation;
+    }
+
+    export interface IlanguageOrOptions {
+        language?: string;
+        preserveExisting?: boolean;
+    }
+
+    export function i18nState(state: IreduxI18nState, action: Action): IreduxI18nState
+
+    export function setLanguage(lang: string): ISetLanguageAction
+
+    export function setTranslations(translations: ITranslations, languageOrOptions?: IlanguageOrOptions | string): ThunkAction<any, IreduxI18nState, any>
+
+    export function setForceRefresh(force: boolean): ISetForceRefreshAction
+
+    export function localize(propName: string): IWrapWithLocalized
+
+    export function getTranslateFunction(translations: ITranslations, lang: string, fallbackLang?: string): IGetTranslateFunctionResponse
+
+    export default class I18n extends React.Component<I18nProps, any> {
+    }
+
+}
